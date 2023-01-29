3785f3637482da50ef5ef769149065c0d193ff7d
Jennifer Faye <jennifer@fayecreative.com>
2023-01-28 19:03:12 -0600

    sticky navbar

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-  });
 
+        // execute navFunction on scroll
+        window.onscroll = function(){navFunction()};
+        // get navbar
+        const navBar = document.getElementById("nav-wrapper");
+        const sideNav = document.getElementById("sidenav-wrapper");
+        // get offset position of navbar
+        const sticky = navBar.offsetTop;
+        const sideStick = sideNav.offsetTop;
+    
+        // reach scroll position - add sticky, leave scroll position - remove sticky
+        function navFunction() {
+          if (window.pageYOffset >= sticky) {
+            navBar.classList.add("sticky")
+          } else {
+            navBar.classList.remove("sticky");
+          }
+          if (window.pageYOffset >= sideStick) {
+            sideNav.classList.add("sideStick")
+          } else {
+            sideNav.classList.remove("sideStick");
+          }
+        }
+  });

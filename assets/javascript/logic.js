6716519218c18ac8faf501daf82df658b8e540e8
Jennifer Faye <jennifer@fayecreative.com>
2023-02-11 22:04:15 -0600

    title animation

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -27,12 +27,12 @@ $(document).ready(function(){
 
         let tl = new TimelineMax();
 
-// tl.staggerFromTo(
-//   ".title",
-//   1.5,
-//   { opacity: 0, x: 120, ease: Back.easeOut.config(5)},
-//   { opacity: 1, x: 0},
-//   1
-// );
+tl.staggerFromTo(
+  ".title",
+  1.5,
+  { opacity: 0, x: 1000, ease: Back.easeOut.config(5)},
+  { opacity: 1, x: 0},
+  1
+);
 
   });

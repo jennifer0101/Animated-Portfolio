43519cefbdb3c367f27a25f8b92b6088a4e00fd1
Jennifer Faye <jennifer@fayecreative.com>
2023-02-11 22:31:20 -0600

    remove sticky nav bar and animation

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,37 +2,16 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
 
-        // execute navFunction on scroll
-        window.onscroll = function(){navFunction()};
-        // get navbar
-        const navBar = document.getElementById("nav-wrapper");
-        const sideNav = document.getElementById("sidenav-wrapper");
-        // get offset position of navbar
-        const sticky = navBar.offsetTop;
-        const sideStick = sideNav.offsetTop;
-    
-        // reach scroll position - add sticky, leave scroll position - remove sticky
-        function navFunction() {
-          if (window.pageYOffset >= sticky) {
-            navBar.classList.add("sticky")
-          } else {
-            navBar.classList.remove("sticky");
-          }
-          if (window.pageYOffset >= sideStick) {
-            sideNav.classList.add("sideStick")
-          } else {
-            sideNav.classList.remove("sideStick");
-          }
-        }
 
-        let tl = new TimelineMax();
 
-tl.staggerFromTo(
-  ".title",
-  1.5,
-  { opacity: 0, x: 1000, ease: Back.easeOut.config(5)},
-  { opacity: 1, x: 0},
-  1
-);
+        // let tl = new TimelineMax();
+
+        // tl.staggerFromTo(
+        //   ".title",
+        //   1.5,
+        //   { opacity: 0, x: 1000, ease: Back.easeOut.config(5)},
+        //   { opacity: 1, x: 0},
+        //   1
+        // );
 
   });

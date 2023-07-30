9215bd224d36bb825a9839588d874c1af41f4e73
Jennifer Faye <jennifer@fayecreative.com>
2023-07-30 17:46:43 -0500

    finalized design

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -102,9 +102,9 @@ $(document).ready(function () {
 
 
 // mobile viewport fix
-  const documentHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
-   }
-   window.addEventListener("resize", documentHeight)
-   documentHeight()
+  // const documentHeight = () => {
+  //   const doc = document.documentElement
+  //   doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+  //  }
+  //  window.addEventListener("resize", documentHeight)
+  //  documentHeight()
